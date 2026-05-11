013ca61805241dd030d0fa709339e3408e7380da
Luis Toledo <luis_toledo35@hotmail.com>
2026-05-11 12:57:57 -0600

    update

--- client/next.config.ts
+++ client/next.config.ts
@@ -11,6 +11,11 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "blog.indumex.blog",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "secure.gravatar.com",
         pathname: "/avatar/**",
       },
